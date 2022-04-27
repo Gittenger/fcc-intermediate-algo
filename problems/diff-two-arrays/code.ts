3a3199a79981669bd0c18b86360c6d89674aef21
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-04-26 20:03:09 -0400

    fix implicit array type

--- problems/diff-two-arrays/code.ts
+++ problems/diff-two-arrays/code.ts
@@ -2,7 +2,7 @@ export const diffArray = (arr1: any[], arr2: any[]): any[] => {
 	// o(n) + o(m) set creation
 	const set1 = new Set(arr1)
 	const set2 = new Set(arr2)
-	const result = []
+	const result: any[] = []
 
 	// o(n) + o(m) iter through each arr
 	arr1.forEach(el => (!set2.has(el) ? result.push(el) : undefined))
