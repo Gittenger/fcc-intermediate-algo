6957332e93a0428dfaa17e13aba808abec74d8f2
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-04-30 04:24:56 -0400

    update group

--- projects/telephone-validator/code.ts
+++ projects/telephone-validator/code.ts
@@ -1,5 +1,5 @@
 export function telephoneCheck(str: string): boolean {
-  const regex = new RegExp(/^1?\s?(\(\d{3}\)|\d{3})(-|\s)?\d{3}[-|\s]?\d{4}$/)
+  const regex = new RegExp(/^1?\s?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/)
   return regex.test(str)
 }
 
